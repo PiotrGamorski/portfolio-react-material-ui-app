370b94ceee430da146f563a0cade73d1d74d390a
Piotr Gamorski <piotr.gamorski@ts.fujitsu.com>
2020-12-17 11:38:50 +0100

    Avataer and icons list added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from "react";
+import { CssBaseline } from "@material-ui/core";
 import "./App.css";
 import Home from "./components";
 
 function App() {
   return (
-    <>
+    <CssBaseline>
       <Home />
-    </>
+    </CssBaseline>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,92 @@
-import React from 'react'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import {
-AppBar,
-Toolbar,
-ListItem,
-IconButton,
-ListItemText,
-Avater,
-Divider,
-Typography,
-Box,
+  AppBar,
+  Toolbar,
+  List,
+  ListItem,
+  IconButton,
+  ListItemText,
+  ListItemIcon,
+  Avatar,
+  Divider,
+  Typography,
+  Box,
 } from "@material-ui/core";
 import {
-    ArrowBack,
-    AssignmentInd,
-    Home,
-    Apps,
-    ContactMail,
-} from "@material-ui/icons"
+  ArrowBack,
+  AssignmentInd,
+  Home,
+  Apps,
+  ContactMail,
+} from "@material-ui/icons";
+import avatar from "../avatar.png";
+
+//CSS STYLES
+const useStyles = makeStyles((theme) => ({
+  menuSliderContainer: {
+    width: 250,
+    background: "#511",
+    height: "30rem",
+  },
+  avatar: {
+    display: "block",
+    margin: "0.5rem auto",
+    width: theme.spacing(13),
+    height: theme.spacing(13),
+  },
+}));
+
+const menuItems = [
+  {
+    listIcon: <Home />,
+    listText: "Home",
+  },
+  {
+    listIcon: <AssignmentInd />,
+    listText: "Resume",
+  },
+  {
+    listIcon: <Apps />,
+    listText: "Portfolio",
+  },
+  {
+    listIcon: <ContactMail />,
+    listText: "Contacts",
+  },
+];
 
 export const Navbar = () => {
-    return (
-        <Box component="nav">
-            <AppBar>
-                <Toolbar>
-                    <ArrowBack/>
-                </Toolbar>
-            </AppBar>
-        </Box>
-    )
-};
+  const classes = useStyles();
 
+  return (
+    <>
+      <Box className={classes.menuSliderContainer} component="div">
+        <Avatar className={classes.avatar} src={avatar} alt="Russel Crowe" />
+        <Divider />
+        <List>
+          {menuItems.map((lsItem, key) => (
+            <ListItem button key={key}>
+              <ListItemIcon>{lsItem.listIcon}</ListItemIcon>
+              <ListItemText primary={lsItem.listText}/>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+      <Box component="nav">
+        <AppBar position="static" style={{ background: "#222" }}>
+          <Toolbar>
+            <IconButton>
+              <ArrowBack style={{ color: "tomato" }} />
+            </IconButton>
+            <Typography variant="h5" style={{ color: "tan" }}>
+              Portfolio
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+};
 
 export default Navbar;
